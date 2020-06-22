36e8276711c824128b4ac4e7d27abd7bb13710cd
Murad Esheilah <mesheilah@thiqah.sa>
2020-06-22 18:10:18 +0300

    my first commit

---
+++ Sources/MyLibrary/Test.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Murad Jamal on 22/06/2020.
+//
+
+import Foundation
+
+public struct Test {
+    public func test() {
+        print("testingggggg")
+    }
+}
